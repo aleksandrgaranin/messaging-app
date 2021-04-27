89fa4f45c3c7a27224783f405ed9b87759d6dbf4
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2021-04-27 14:44:10 -0700

    custom hook for localStorage manipulation

--- client-messaging-app/src/components/App.js
+++ client-messaging-app/src/components/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React  from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 
 import Login from "./Login";
 
 function App() {
-  const [id, setId] = useState();
+  const [id, setId] = useLocalStorage();
   return (
     <>
       {id}
