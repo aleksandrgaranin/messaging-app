7460081f9a680107d8bb89e63fb184fb6789fc97
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2021-04-28 18:59:19 -0700

    complete new convesation modal + Conversation provider + save to the local storage + display conversations

---
+++ client-messaging-app/src/contexts/ConversationsProvider.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { useContacts } from "./ContactsProvider";
+
+const ConversationsContext = React.createContext();
+
+export const useConversations = () => {
+  return useContext(ConversationsContext);
+};
+
+export const ConversationsProvider = ({ children }) => {
+  const [conversations, setConversations] = useLocalStorage(
+    "conversations",
+    []
+  );
+  const { contacts } = useContacts();
+
+  const createConversation = (recipients) => {
+    setConversations((prevConversation) => {
+      return [...prevConversation, { recipients, messages: [] }];
+    });
+  };
+
+  const formattedConversations = conversations.map((conversation) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === recipient;
+      });
+      const name = (contact && contact.name) || recipient;
+      return { id: recipient, name };
+    });
+
+    return { ...conversation, recipients };
+  });
+
+  const value = {
+    conversations: formattedConversations,
+    createConversation
+  };
+
+  return (
+    <ConversationsContext.Provider value={ value }>
+      {children}
+    </ConversationsContext.Provider>
+  );
+};
+
+export default ConversationsProvider;
