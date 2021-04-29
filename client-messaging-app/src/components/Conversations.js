ed1aaa635c821a3ba4e1f8d834d32739d9fdd8e2
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2021-04-28 20:48:15 -0700

    Open Conversation tab completed

--- client-messaging-app/src/components/Conversations.js
+++ client-messaging-app/src/components/Conversations.js
@@ -3,12 +3,19 @@ import { ListGroup } from "react-bootstrap";
 import { useConversations } from "../contexts/ConversationsProvider";
 
 const Conversations = () => {
-  const { conversations } = useConversations();
+  const { conversations, selectConversationIndex } = useConversations();
 
   return (
     <ListGroup variant="flush">
       {conversations.map((conversation, index) => (
-        <ListGroup.Item key={index}>{conversation.recipients.map(r => r.name).join(', ')}</ListGroup.Item>
+        <ListGroup.Item
+          key={index}
+          action
+          onClick={() => selectConversationIndex(index)}
+          active={conversation.selected}
+        >
+          {conversation.recipients.map((r) => r.name).join(", ")}
+        </ListGroup.Item>
       ))}
     </ListGroup>
   );
